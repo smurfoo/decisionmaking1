d22731a99e9fdfdb14d9c5f85416eae058cab421
Ilyas Guleed <iguleed1@nait.ca>
2022-09-14 09:08:13 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿/*  
+ Purpose: Write a program that will prompt for a number and display “positive” if it is greater than zero,
+“negative” if it is less than zero, and “zero” if it is equal to zero.
+Input: number
+Output: stringmessage
+ Author: Ilyas G
+Date: Sept 14 2022
+ */
+
+
+
+
+namespace decisionmaking1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // declare variables
+            int number;
+            string message;
+
+            // input a number
+            Console.Write("Input a number: ");
+            number = int.Parse(Console.ReadLine());
+
+            // determine if it positive negative or equal to zero
+          if (number > 0)
+            {
+                message = "positive";
+            }
+            else
+            {
+                if (number < 0)
+                {
+                    message = "negative";
+                }
+                else
+                {
+                    message = "zero";
+                }
+            }
+
+
+            // display message
+            Console.WriteLine($"The number, {number}, is {message}");
+
+        }
+    }
+}
